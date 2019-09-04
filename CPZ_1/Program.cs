7ef7414b723ce0bab4607bea956c86e91c9d3e57
cr0manty <36458874+cr0manty@users.noreply.github.com>
2019-09-05 01:16:07 +0300

    initial commit

---
+++ CPZ_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPZ_1
+{
+    class Program
+    {
+        static BankBill input()
+        {
+            string full_name;
+            string bill_number;
+            float amount;
+
+            Console.WriteLine("Enter full name:");
+            full_name = Console.ReadLine();
+            Console.WriteLine("Enter biill number(8 digits):");
+            bill_number = Console.ReadLine();
+            Console.WriteLine("Enter the amount in the bill:");
+            amount = Console.Read();
+
+            return new BankBill(full_name, bill_number, amount);
+        }
+
+        static void Main(string[] args)
+        {
+            BankBill bill = input();
+
+            Console.Read();
+        }
+    }
+}
